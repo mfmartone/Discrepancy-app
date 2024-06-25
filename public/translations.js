2df270d5b32472c7f712252f0a70b09f5386c709
mfmartone <138795479+mfmartone@users.noreply.github.com>
2024-06-25 11:26:22 +0200

    Add files via upload

---
+++ public/translations.js
@@ -0,0 +1,20 @@
+const translations = {
+    en: {
+        title: "Discrepancy Report Generator",
+        selectFile: "Select the Excel file",
+        removeFile: "Remove",
+        uploadProcess: "Upload and Process",
+        downloadTemplate: "Download Template",
+        progressBar: "Processing...",
+        result: "Result"
+    },
+    it: {
+        title: "Generatore di Rapporti di Discrepanze",
+        selectFile: "Seleziona il file Excel",
+        removeFile: "Rimuovi",
+        uploadProcess: "Carica e Processa",
+        downloadTemplate: "Scarica il Template",
+        progressBar: "Elaborazione...",
+        result: "Risultato"
+    }
+};
